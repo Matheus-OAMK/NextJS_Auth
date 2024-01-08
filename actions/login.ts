58345d5e22247feb351945c5cd1c7d7283c0d99b
Matheus-OAMK <t2fema00@students.oamk.fi>
2024-01-08 19:42:36 +0200

    Database connection and Prisma user schema

--- actions/login.ts
+++ actions/login.ts
@@ -2,11 +2,8 @@
 import * as z from "zod"
 import { LoginSchema } from "@/schemas"
 
-
-
 export const login = async (values: z.infer<typeof LoginSchema>) => {
   const validatedFields = LoginSchema.safeParse(values)
- 
 
   if (validatedFields.success) {
     return { error: "Invalid fields!" }
--- actions/register.ts
+++ actions/register.ts
@@ -1,13 +1,39 @@
 "use server"
 import * as z from "zod"
+import bcrypt from "bcrypt"
+
+import { db } from "@/lib/db"
 import { RegisterSchema } from "@/schemas"
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values)
 
-  if (validatedFields.success) {
+  if (!validatedFields.success) {
     return { error: "Invalid fields!" }
   }
 
-  return { success: "Email sent!" }
+  const { email, password, name } = validatedFields.data
+  const hashedPassword = await bcrypt.hash(password, 10)
+
+  const existingUser = await db.user.findUnique({
+    where: {
+      email,
+    },
+  })
+
+  if (existingUser) {
+    return { error: "User already exists!" }
+  }
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  })
+
+  // TODO: Send email confirmation
+
+  return { success: "User created!" }
 }
