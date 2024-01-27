2b82c722cdbd9379e41478c4edd6f8ef7f3c60a6
Matheus Oliveira <t2fema00@students.oamk.fi>
2024-01-27 21:24:51 +0200

    Two-Factor: Lib function to create twoFactorToken

--- lib/tokens.ts
+++ lib/tokens.ts
@@ -1,14 +1,41 @@
+import { v4 as uuidv4 } from "uuid"
+import crypto from "crypto"
+
+import { db } from "@/lib/db"
 import { getVerificationTokenByEmail } from "@/data/verification-token"
 import { getPasswordResetTokenByEmail } from "@/data/password-reset-token"
-import { v4 as uuidv4 } from "uuid"
-import { db } from "@/lib/db"
+import { getTwoFactorTokenByEmail } from "@/data/two-factor-token"
 
 
-export const generatePasswordResetToken = async ( email: string) => {
+
+export const generateTwoFactorToken = async (email: string) => {
+  const token = crypto.randomInt(100_000, 1_000_000).toString()
+  const expiresAt = new Date(new Date().getTime() + 3600 * 1000) // Expires in 1hr
+
+  const existingToken = await getTwoFactorTokenByEmail(email)
+
+  if (existingToken) {
+    await db.twoFactorToken.delete({
+      where: { id: existingToken.id },
+    })
+  }
+
+
+  const twoFactorToken = await db.twoFactorToken.create({
+    data: {
+      email,
+      token,
+      expiresAt
+    }
+  })
+
+  return twoFactorToken
+}
+
+export const generatePasswordResetToken = async (email: string) => {
   const token = uuidv4()
   const expiresAt = new Date(new Date().getTime() + 3600 * 1000) // Expires in 1hr
 
-  
   const existingToken = await getPasswordResetTokenByEmail(email)
 
   // Delete existing token if it exists
@@ -29,9 +56,6 @@ export const generatePasswordResetToken = async ( email: string) => {
 
   return passwordResetToken
 }
-
-
-
 
 export const generateVerificationToken = async (email: string) => {
   const token = uuidv4()
