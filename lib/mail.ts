e1f5593a5dba4592629a22417ddb00f882d6a031
Matheus Oliveira <t2fema00@students.oamk.fi>
2024-01-27 21:28:19 +0200

    Two-Factor: Lib function to send 2FA code by email

--- lib/mail.ts
+++ lib/mail.ts
@@ -16,8 +16,7 @@ export const sendVerificationEmail = async (email: string, token: string) => {
   })
 }
 
-
-export const sendPasswodResetEmail = async (email: string , token:string) => {
+export const sendPasswodResetEmail = async (email: string, token: string) => {
   const resetLink = `http://localhost:3000/auth/new-password?token=${token}`
 
   await resend.emails.send({
@@ -27,6 +26,17 @@ export const sendPasswodResetEmail = async (email: string , token:string) => {
     html: `
     <h1>Confirm password reset</h1>
     <p>Click <a href="${resetLink}"> here <a/> to reset your password.</p>
-    `
+    `,
+  })
+}
+
+export const sendTwoFactorTokenEmail = async (email: string, token: string) => {
+  await resend.emails.send({
+    from: "onboarding@resend.dev",
+    to: email,
+    subject: "2FA Code",
+    html: `
+    <h1>Your 2FA code: ${token}
+    `,
   })
 }
