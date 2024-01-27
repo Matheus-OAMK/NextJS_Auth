20c24bb25bb838753db8d0ce5b868941e50cf3c3
Matheus Oliveira <t2fema00@students.oamk.fi>
2024-01-27 16:12:09 +0200

    Password reset token generation function

--- lib/tokens.ts
+++ lib/tokens.ts
@@ -1,6 +1,38 @@
 import { getVerificationTokenByEmail } from "@/data/verification-token"
+import { getPasswordResetTokenByEmail } from "@/data/password-reset-token"
 import { v4 as uuidv4 } from "uuid"
 import { db } from "@/lib/db"
+
+
+export const generatePasswordResetToken = async ( email: string) => {
+  const token = uuidv4()
+  const expiresAt = new Date(new Date().getTime() + 3600 * 1000) // Expires in 1hr
+
+  
+  const existingToken = await getPasswordResetTokenByEmail(email)
+
+  // Delete existing token if it exists
+  if (existingToken) {
+    await db.verificationToken.delete({
+      where: { id: existingToken.id },
+    })
+  }
+
+  // Create new token in db and return it
+  const passwordResetToken = await db.verificationToken.create({
+    data: {
+      email,
+      token,
+      expiresAt,
+    },
+  })
+
+  return passwordResetToken
+}
+
+
+
+
 export const generateVerificationToken = async (email: string) => {
   const token = uuidv4()
   const expiresAt = new Date(new Date().getTime() + 3600 * 1000) // Expires in 1hr
