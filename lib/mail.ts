55409da112564e7ba5273f2aebbae228e0fa2be6
Matheus Oliveira <t2fema00@students.oamk.fi>
2024-01-27 16:18:01 +0200

    Password reset: Function to send reset password link

--- lib/mail.ts
+++ lib/mail.ts
@@ -11,7 +11,22 @@ export const sendVerificationEmail = async (email: string, token: string) => {
     subject: "Confirm your email",
     html: `
       <h1>Confirm your email</h1>
-      <p>Click <a href="${confirmLink}">here</a> to confirm your email.</p>
+      <p>Click <a href="${confirmLink}">here </a> to confirm your email.</p>
     `,
+  })
+}
+
+
+export const sendPasswodResetEmail = async (email: string , token:string) => {
+  const resetLink = `http://localhost:300/auth/new-password?token=${token}`
+
+  await resend.emails.send({
+    from: "onboarding@resend.dev",
+    to: email,
+    subject: "Reset your password",
+    html: `
+    <h1>Confirm password reset</h1>
+    <p>Click <a href="${resetLink}"> here <a/> to reset your password.</p>
+    `
   })
 }
