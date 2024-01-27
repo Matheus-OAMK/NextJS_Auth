a76f9d000d895231b81eed2f58c63ae71d4adbc0
Matheus Oliveira <t2fema00@students.oamk.fi>
2024-01-27 16:25:00 +0200

    Password reset: Fix typo on sendPasswordResetEmail to correct localhost port

--- lib/mail.ts
+++ lib/mail.ts
@@ -18,7 +18,7 @@ export const sendVerificationEmail = async (email: string, token: string) => {
 
 
 export const sendPasswodResetEmail = async (email: string , token:string) => {
-  const resetLink = `http://localhost:300/auth/new-password?token=${token}`
+  const resetLink = `http://localhost:3000/auth/new-password?token=${token}`
 
   await resend.emails.send({
     from: "onboarding@resend.dev",
