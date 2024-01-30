96bbbc76507d7be38393c972ae4d9a971b19e32b
Matheus Oliveira <t2fema00@students.oamk.fi>
2024-01-30 21:14:48 +0200

    Theme: Toggle Dark/Light mode button

--- components/toggle-mode-button.tsx
+++ components/toggle-mode-button.tsx
@@ -21,9 +21,10 @@ export const ToggleModeButton = () => {
         <Button
           variant="outline"
           size="icon"
+          className="sticky bottom-10 left-10"
         >
-          <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <SunIcon className="h-[1.5rem] w-[1.5rem] rotate-0 scale-100 transition-all dark:-rotate-90  dark:scale-0" />
+          <MoonIcon className="absolute h-[1.5rem] w-[1.5rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
